9f234b6b44340fc786e12c9f2a3181b20a33559b
Mason France <masonrf1@gmail.com>
2019-01-05 16:36:39 -0700

    Add input alert, hover css, and connect local storage

--- main.js
+++ main.js
@@ -15,24 +15,29 @@ var photoArray = [];
 addBtn.addEventListener('click', createCard);
 // favoriteBtn.addEventListener();
 
-// window.onload = function() {
-//   var keys = Object.keys(localStorage);
-//     keys.forEach (key =>{
-//     var parseObj = JSON.parse(localStorage.getItem(key));
-//     newCard = new Photo(parseObj.id, parseObj.title, parseObj.caption, parseObj.fav);
-//     photoArray.push(newPhoto);
-//     appendCard(newPhoto); 
-//   })
-// }
+window.onload = function() {
+  var keys = Object.keys(localStorage);
+    keys.forEach (key =>{
+    var parseObj = JSON.parse(localStorage.getItem(key));
+    newPhoto = new Photo(parseObj.id, parseObj.title, parseObj.caption, parseObj.fav);
+    photoArray.push(newPhoto);
+    appendCard(newPhoto); 
+  })
+}
 
 function createCard (event) {
   event.preventDefault();
   var titleInput = document.querySelector('.title-input');
   var captionInput = document.querySelector('.caption-input');
-  const newPhoto = new Photo(Date.now(), titleInput.value, captionInput.value)
-  newPhoto.saveToStorage();
-  photoArray.push(newPhoto);
-  appendCard(newPhoto);
+  const newPhoto = new Photo(Date.now(), titleInput.value, captionInput.value);
+  if (captionInput.value !== "" && titleInput.value !== "") {
+    newPhoto.saveToStorage();
+    photoArray.push(newPhoto);
+    appendCard(newPhoto);
+  } else {
+    alert('Please enter a photo title and caption.');
+    return false;
+  }
 }
 
 function appendCard(photo) {
@@ -46,11 +51,24 @@ function appendCard(photo) {
         <p class="card-body-text">${photo.caption}</p>
       </section>
       <footer class="card-foot">
-        <img src="images/delete.svg">
+        <img src="images/delete.svg" onclick="deleteCard(${photo.id})">
         <img src="images/favorite.svg">
       </footer>
     </article>`
     photoArea.innerHTML = card + photoArea.innerHTML;
+}
+
+function deleteCard (id) {
+  let element = document.querySelector(`[data-id="${id}"]`);
+  element.remove();
+  let deleteIdea = photoArray.find(function(photo) {
+    return id === photo.id;
+  });
+  deleteIdea.deleteFromStorage();
+  let deleteIndex = photoArray.findIndex(function(photo) {
+    return id === photo.id;
+  });
+  photoArray.splice(deleteIndex, 1)
 }
 
 
