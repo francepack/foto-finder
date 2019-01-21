985e746dba0c41ebab3b0b27acfa70058511535d
Mason France <masonrf1@gmail.com>
2019-01-21 14:23:02 -0700

    Refactor for Mod 2 - Make JQuery attempts and eslint package add

--- main.js
+++ main.js
@@ -1,28 +1,28 @@
 //Button and Input grabs
-var searchInput = document.querySelector(".search-bar");
-var searchBtn = document.querySelector(".search-btn");
-var titleInput = document.querySelector('.title-input');
-var captionInput = document.querySelector('.caption-input');
-var chooseBtn = document.querySelector(".choose-btn");
-var favoriteBtn = document.querySelector(".favorite-btn");
-var addBtn = document.querySelector(".add-btn");
-var photoArea = document.querySelector(".photo-card-area");
-var photoArray = [];
-var reader = new FileReader();
-var buttonCount = 0;
-var favNum = document.querySelector(".fav-num");
+const searchInput = document.querySelector(".search-bar");
+const searchBtn = document.querySelector(".search-btn");
+const titleInput = document.querySelector('.title-input');
+const captionInput = document.querySelector('.caption-input');
+const chooseBtn = document.querySelector(".choose-btn");
+const favoriteBtn = document.querySelector(".favorite-btn");
+// const addBtn = document.querySelector(".add-btn");
+const photoArea = document.querySelector(".photo-card-area");
+let photoArray = [];
+let reader = new FileReader();
+let buttonCount = 0;
+const favNum = document.querySelector(".fav-num");
 
 //Event Listeners
-searchInput.addEventListener('keyup', searchPhotos);
-addBtn.addEventListener('click', createElement);
-favoriteBtn.addEventListener('click', filterFavorites);
-photoArea.addEventListener('click', updateText);
+$(".search-bar").on('keyup', searchPhotos);
+$(".add-btn").on('click', createElement);
+$(".favorite-btn").on('click', filterFavorites);
+$(".photo-card-area").on('click', updateText);
 
 
 window.onload = function() {
-  var keys = Object.keys(localStorage);
-    keys.forEach (function(key) {
-    var parseObj = JSON.parse(localStorage.getItem(key));
+  let keys = Object.keys(localStorage);
+    keys.forEach (key => {
+    let parseObj = JSON.parse(localStorage.getItem(key));
     newPhoto = new Photo(parseObj.id, parseObj.title, parseObj.caption, parseObj.file, parseObj.favorite);
     photoArray.push(newPhoto);
     appendCard(newPhoto); 
@@ -32,7 +32,7 @@ window.onload = function() {
 }
 
 function countFavorites() {
-  photoArray.forEach(function(count) {
+  photoArray.forEach(count => {
     if (count.favorite === true) {
     buttonCount += 1;
     }
@@ -61,7 +61,7 @@ function createCard (e) {
 }
 
 function findIndexNumber(objId) {
-  for (var i = 0; i < photoArray.length; i++) {
+  for (let i = 0; i < photoArray.length; i++) {
     if (photoArray[i].id === objId) {
       console.log(i);
       return i;
@@ -70,7 +70,7 @@ function findIndexNumber(objId) {
 }
 
 function changeFavorite(id) {
-  var cardIndex = findIndexNumber(id);
+  let cardIndex = findIndexNumber(id);
   if (photoArray[cardIndex].favorite === false) {
     photoArray[cardIndex].favorite = true;
     event.target.src = "images/favorite-active.svg";
@@ -92,14 +92,14 @@ function updateButtonCount() {
 function filterFavorites(e) {
   if (favoriteBtn.innerHTML === 'View All') {
     clearCards();
-    photoArray.forEach(function(photo) {
+    photoArray.forEach(photo => {
       appendCard(photo);
     })
-    favoriteBtn.innerHTML = `View ${buttonCount} Favorites`;
+    $(".favorite-btn").html(`View ${buttonCount} Favorites`);
     } else {
     e.preventDefault();
     clearCards();
-    photoArray.forEach(function(photo) {
+    photoArray.forEach(photo => {
       trueCheck(photo);
     })
   }
@@ -108,18 +108,18 @@ function filterFavorites(e) {
 function trueCheck(photo) {
   if (photo.favorite === true) {
     appendCard(photo);
-    favoriteBtn.innerHTML = "View All"
+    $(".favorite-btn").html("View All")
   }
 }
 
 function appendCard(photo, fav) {
-  var favoriteImg;
+  let favoriteImg;
   if(photo.favorite === true) {
     favoriteImg = "images/favorite-active.svg";
   } else {
     favoriteImg = "images/favorite.svg";
   }
-  var card = 
+  let card = 
     `<article class="card" data-id="${photo.id}">
       <header class="card-head">
         <h4 class="card-head-text" contenteditable="true">${photo.title}</h4>
@@ -137,15 +137,15 @@ function appendCard(photo, fav) {
 }
 
 function deleteCard (id) {
-  var result = confirm("Are you sure you want to delete this photograph?");
+  let result = confirm("Are you sure you want to delete this photograph?");
   if (result) {
-    var element = document.querySelector(`[data-id="${id}"]`);
+    let element = document.querySelector(`[data-id="${id}"]`);
     element.remove();
-    var deleteIdea = photoArray.find(function(photo) {
+    let deleteIdea = photoArray.find(photo => {
       return id === photo.id;
     });
     deleteIdea.deleteFromStorage();
-    var deleteIndex = photoArray.findIndex(function(photo) {
+    let deleteIndex = photoArray.findIndex(photo => {
       return id === photo.id;
     });
     photoArray.splice(deleteIndex, 1)
@@ -154,21 +154,21 @@ function deleteCard (id) {
 
 function searchPhotos (e) {
   e.preventDefault();
-  var searchWord = searchInput.value.toUpperCase();
-  var filteredPhotos = photoArray.filter(function(obj) {
-    var titleText = obj.title.toUpperCase();
-    var captionText = obj.caption.toUpperCase();
+  let searchWord = searchInput.value.toUpperCase();
+  let filteredPhotos = photoArray.filter(obj => {
+    let titleText = obj.title.toUpperCase();
+    let captionText = obj.caption.toUpperCase();
     return titleText.includes(searchWord) || captionText.includes(searchWord);
   });
   photoArea.innerHTML = "";
-  filteredPhotos.forEach(function(obj) {
+  filteredPhotos.forEach(obj => {
     appendCard(obj)
   })
 }
 
 function clearCards() {
-  var allCards = document.querySelectorAll('.card');
-  allCards.forEach(function(card) {
+  const allCards = document.querySelectorAll('.card');
+  allCards.forEach(card => {
     card.remove();
   })
 }
@@ -179,7 +179,7 @@ function clearInput() {
 }
 
 function updateText(e) {
-  var cardIndex = findIndexNumber(parseInt(e.target.parentElement.parentElement.dataset.id));
+  let cardIndex = findIndexNumber(parseInt(e.target.parentElement.parentElement.dataset.id));
   console.log(e.target.parentElement.parentElement.dataset.id);
     if (e.target.classList.contains('card-head-text')) {
       photoArray[cardIndex].updatePhoto(e.target.innerText, 'card-head-text');
